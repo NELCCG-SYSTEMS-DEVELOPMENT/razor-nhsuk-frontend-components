a4afbec7ce22933812cbed6d97cf01ce004c5075
Sam Collett <sam.collett@nhs.net>
2021-06-17 15:27:07 +0100

    feat: create id generator helper

---
+++ SamNHS.NHSUKFrontend.Razor/Helpers/IdGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace SamNHS.NHSUKFrontend.Razor.Helpers
+{
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using System;
+    using System.Security.Cryptography;
+
+    public class IdGenerator
+    {
+        private static readonly RNGCryptoServiceProvider rngCsp = new();
+
+        public static string GenerateId()
+        {
+            return GenerateId(string.Empty);
+        }
+
+        public static string GenerateId(string prefix, string suffix = "")
+        {
+            byte[] key = new byte[6];
+            rngCsp.GetBytes(key);
+            return TagBuilder.CreateSanitizedId($"{prefix}{Convert.ToBase64String(key)}{suffix}", "");
+        }
+    }
+}
