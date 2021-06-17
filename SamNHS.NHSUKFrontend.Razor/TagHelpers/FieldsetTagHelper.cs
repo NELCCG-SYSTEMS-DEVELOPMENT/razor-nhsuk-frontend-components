494554c65ec33fe0f60b5617463a4d2f6e22100d
Sam Collett <sam.collett@nhs.net>
2021-06-17 16:02:05 +0100

    feat: add fieldset tag helper

---
+++ SamNHS.NHSUKFrontend.Razor/TagHelpers/FieldsetTagHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace SamNHS.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("nhsuk-fieldset")]
+    public class FieldsetTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "fieldset";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.AddClass("nhsuk-fieldset", HtmlEncoder.Default);
+        }
+    }
+}
