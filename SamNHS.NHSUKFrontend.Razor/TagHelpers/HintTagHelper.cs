9ac0cc0b1a55a592cde453cd946f4742dae7b7ab
Sam Collett <sam.collett@nhs.net>
2021-06-17 15:58:49 +0100

    feat: add hint tag helper

---
+++ SamNHS.NHSUKFrontend.Razor/TagHelpers/HintTagHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace SamNHS.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("nhsuk-hint")]
+    public class HintTagHelper : TagHelper
+    {
+        /// <summary>
+        /// When this is set, the hint text is encoded
+        /// </summary>
+        public string Text { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.AddClass("nhsuk-hint", HtmlEncoder.Default);
+            if (context.Items.TryGetValue("hint-class", out object hintClass))
+            {
+                output.AddClass(hintClass.ToString(), HtmlEncoder.Default);
+            }
+            if (!string.IsNullOrWhiteSpace(this.Text))
+            {
+                output.Content.SetContent(this.Text);
+            }
+        }
+    }
+}
