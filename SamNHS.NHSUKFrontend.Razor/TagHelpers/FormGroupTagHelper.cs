ab2b12e478e225442bd7c4c747813736991f5d28
Sam Collett <sam.collett@nhs.net>
2021-06-17 15:52:41 +0100

    feat: add form group tag helper

---
+++ SamNHS.NHSUKFrontend.Razor/TagHelpers/FormGroupTagHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace SamNHS.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("nhsuk-form-group")]
+    public class FormGroupTagHelper : TagHelper
+    {
+        public bool ContainsErrorMessage { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.AddClass("nhsuk-form-group", HtmlEncoder.Default);
+            if (this.ContainsErrorMessage)
+            {
+                output.AddClass("nhsuk-form-group--error", HtmlEncoder.Default);
+            }
+        }
+    }
+}
