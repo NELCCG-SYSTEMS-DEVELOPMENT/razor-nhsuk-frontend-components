e83683ccef8ae499b61c70094295b51dfda76b3f
Sam Collett <sam.collett@nhs.net>
2021-06-18 19:30:35 +0100

    feat: add text input tag helper

---
+++ SamNHS.NHSUKFrontend.Razor/TagHelpers/TextInputTagHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace SamNHS.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+
+    [HtmlTargetElement("nhsuk-input")]
+    public class TextInputTagHelper : FormInputElementTagHelperBase
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+
+            output.TagName = "input";
+            output.TagMode = TagMode.SelfClosing;
+            output.Attributes.SetAttribute("type", "text");
+            output.AddClass("nhsuk-input", HtmlEncoder.Default);
+
+            if (this.For != null)
+            {
+                if (this.For.Metadata.ModelType == typeof(int) || this.For.Metadata.ModelType == typeof(uint))
+                {
+                    output.Attributes.SetAttribute("inputmode", "numeric");
+                    output.Attributes.SetAttribute("pattern", "[0-9]*");
+                }
+            }
+
+            var childContent = await output.GetChildContentAsync();
+            output.PreElement.AppendHtml(childContent);
+        }
+    }
+}
