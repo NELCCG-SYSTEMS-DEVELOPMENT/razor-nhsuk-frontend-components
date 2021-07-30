3e7cafccd0bb2584a343f211a36f093e0f019ea3
Sam Collett <sam.collett@nhs.net>
2021-07-30 13:13:21 +0100

    feat: add tag helper for link and script tags.
    Allows linking to version 4.1.0 and 5.1.0 of the frontend CSS/JS.
    
    `<link nhsuk-frontend-version="Version__5_1_0" />`
    `<script nhsuk-frontend-version="Version__5_1_0"></script>`

---
+++ SamNHS.NHSUKFrontend.Razor/TagHelpers/FrontendLinkTagHelper.cs
@@ -0,0 +1,45 @@
+﻿namespace SamNHS.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+    using Microsoft.AspNetCore.Mvc.Routing;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using SamNHS.NHSUKFrontend.Razor.Models;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("link")]
+    public class FrontendLinkTagHelper : UrlResolutionTagHelper
+    {
+        public FrontendLinkTagHelper(IUrlHelperFactory urlHelperFactory, HtmlEncoder htmlEncoder) : base(urlHelperFactory, htmlEncoder)
+        {
+        }
+
+        [HtmlAttributeName("nhsuk-frontend-version")]
+        public FrontendOptions.FrontendVersion Version { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!context.AllAttributes.ContainsName("nhsuk-frontend-version")) return;
+
+            var ns = System.IO.Path.GetFileNameWithoutExtension(this.GetType().Module.Name);
+            output.Attributes.SetAttribute("rel", "stylesheet");
+
+            switch (Version)
+            {
+                case FrontendOptions.FrontendVersion.Version__4_1_0:
+                    output.Attributes.Add("href", this.UrlHelperFactory.GetUrlHelper(this.ViewContext).Content(
+                        $"~/_content/{ns}/css/nhsuk-4.1.0.min.css"
+                    ));
+                    
+                    break;
+                case FrontendOptions.FrontendVersion.Version__5_1_0:
+                    output.Attributes.Add("href", this.UrlHelperFactory.GetUrlHelper(this.ViewContext).Content(
+                        $"~/_content/{ns}/css/nhsuk-5.1.0.min.css"
+                    ));
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ SamNHS.NHSUKFrontend.Razor/TagHelpers/FrontendScriptTagHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace SamNHS.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+    using Microsoft.AspNetCore.Mvc.Routing;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using SamNHS.NHSUKFrontend.Razor.Models;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("script")]
+    public class FrontendScriptTagHelper : UrlResolutionTagHelper
+    {
+        public FrontendScriptTagHelper(IUrlHelperFactory urlHelperFactory, HtmlEncoder htmlEncoder) : base(urlHelperFactory, htmlEncoder)
+        {
+        }
+
+        [HtmlAttributeName("nhsuk-frontend-version")]
+        public FrontendOptions.FrontendVersion Version { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!context.AllAttributes.ContainsName("nhsuk-frontend-version")) return;
+
+            var ns = System.IO.Path.GetFileNameWithoutExtension(this.GetType().Module.Name);
+
+            switch (Version)
+            {
+                case FrontendOptions.FrontendVersion.Version__4_1_0:
+                    output.Attributes.Add("src", this.UrlHelperFactory.GetUrlHelper(this.ViewContext).Content(
+                        $"~/_content/{ns}/js/nhsuk-4.1.0.min.js"
+                    ));
+                    
+                    break;
+                case FrontendOptions.FrontendVersion.Version__5_1_0:
+                    output.Attributes.Add("src", this.UrlHelperFactory.GetUrlHelper(this.ViewContext).Content(
+                        $"~/_content/{ns}/js/nhsuk-5.1.0.min.js"
+                    ));
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+    }
+}
