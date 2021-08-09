ce40c8182305a49922d9a88b3cf6c148b016c1f8
Sam Collett <sam.collett@nhs.net>
2021-08-09 18:23:15 +0100

    docs: example for care card usage

---
+++ NELCCG.NHSUKFrontend.MvcApp/Controllers/ContentController.cs
@@ -0,0 +1,17 @@
+﻿namespace NELCCG.NHSUKFrontend.MvcApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CareCard()
+        {
+            return View();
+        }
+    }
+}
