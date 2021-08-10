aa3bb7f354b75b9ea5017ef2a710a8d719af6b27
Sam Collett <sam.collett@nhs.net>
2021-08-10 12:31:06 +0100

    feat: add contents list tag helpers

---
+++ NELCCG.NHSUKFrontend.Razor/TagHelpers/ContentsListItemTagHelper.cs
@@ -0,0 +1,62 @@
+﻿namespace NELCCG.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("nhsuk-contents-list-item", ParentTag = "nhsuk-contents-list")]
+    public class ContentsListItemTagHelper : TagHelper
+    {
+        /// <summary>
+        /// Set the <c>aria-current</c> attribute. Default is <c>"page"</c>, but may be other values, like <c>"step"</c>.
+        /// </summary>
+        public string AriaCurrent { get; set; } = "page";
+        /// <summary>
+        /// Is the current active item.
+        /// </summary>
+        public bool Current { get; set; }
+        /// <summary>
+        /// Link for the list item. Not used if <see cref="Current"/> is set to <c>true</c>.
+        /// </summary>
+        public string Href { get; set; }
+        /// <summary>
+        /// Text to display for the list item.
+        /// </summary>
+        public string Text { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";
+            output.AddClass("nhsuk-contents-list__item", HtmlEncoder.Default);
+
+            if (this.Current)
+            {
+                var spanTag = new TagBuilder("span");
+                spanTag.AddCssClass("nhsuk-contents-list__current");
+                spanTag.InnerHtml.Append(this.Text);
+                output.Attributes.Add("aria-current", this.AriaCurrent);
+                output.Content.AppendHtml(spanTag);
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(this.Href))
+                {
+                    throw new ArgumentException("Href property is null or an empty string");
+                }
+                else if (!this.Href.StartsWith("#") && !Uri.IsWellFormedUriString(this.Href, UriKind.RelativeOrAbsolute))
+                {
+                    throw new FormatException("Invalid format for Href");
+                }
+
+                var anchorTag = new TagBuilder("a");
+                anchorTag.AddCssClass("nhsuk-contents-list__link");
+                anchorTag.Attributes.Add("href", this.Href);
+                anchorTag.InnerHtml.Append(this.Text);
+                output.Content.AppendHtml(anchorTag);
+                
+            }
+        }
+    }
+}
---
+++ NELCCG.NHSUKFrontend.Razor/TagHelpers/ContentsListTagHelper.cs
@@ -0,0 +1,65 @@
+﻿namespace NELCCG.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System;
+    using System.Collections.Generic;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+
+    [HtmlTargetElement("nhsuk-contents-list")]
+    [RestrictChildren("nhsuk-contents-list-item")]
+    public class ContentsListTagHelper : TagHelper
+    {
+        public string AriaLabel { get; set; }
+        /// <summary>
+        /// Any additional css classes to apply.
+        /// </summary>
+        [HtmlAttributeName("classes")]
+        public IEnumerable<string> CssClasses { get; set; } = Array.Empty<string>();
+        /// <summary>
+        /// Set the hidden heading level.
+        /// </summary>
+        public int HiddenHeadingLevel { get; set; } = 2;
+        /// <summary>
+        /// Set the hidden heading text.
+        /// </summary>
+        public string HiddenHeadingText { get; set; } = "Contents";
+        
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "nav";
+            output.AddClass("nhsuk-contents-list", HtmlEncoder.Default);
+
+            foreach (var cssClass in this.CssClasses)
+            {
+                output.AddClass(cssClass, HtmlEncoder.Default);
+            }
+
+            output.Attributes.Add("role", "navigation");
+
+            if (!string.IsNullOrWhiteSpace(this.AriaLabel))
+            {
+                output.Attributes.Add("aria-label", this.AriaLabel);
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.HiddenHeadingText))
+            {
+                var headingTag = new TagBuilder($"h{this.HiddenHeadingLevel}");
+                headingTag.AddCssClass("nhsuk-u-visually-hidden");
+                headingTag.InnerHtml.Append(this.HiddenHeadingText);
+                output.Content.AppendHtml(headingTag);
+            }
+
+            var contentContainer = new TagBuilder("ol");
+            contentContainer.AddCssClass("nhsuk-contents-list__list");
+
+            var content = await output.GetChildContentAsync();
+            contentContainer.InnerHtml.AppendHtml(content);
+
+            output.Content.AppendHtml(contentContainer);
+        }
+    }
+}
