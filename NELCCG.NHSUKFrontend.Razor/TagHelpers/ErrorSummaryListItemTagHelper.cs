8ad4c3c62c7fd4086a5177db79aef90fe7ee9704
Sam Collett <sam.collett@nhs.net>
2021-08-10 18:07:38 +0100

    feat: add error summary tag helpers

---
+++ NELCCG.NHSUKFrontend.Razor/TagHelpers/ErrorSummaryListItemTagHelper.cs
@@ -0,0 +1,45 @@
+﻿namespace NELCCG.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System;
+    using System.Threading.Tasks;
+
+    [HtmlTargetElement("nhsuk-error-summary-list-item", ParentTag = "nhsuk-error-summary")]
+    public class ErrorSummaryListItemTagHelper : TagHelper
+    {
+        public string Href { get; set; }
+        /// <summary>
+        /// Text to display for the list item.
+        /// </summary>
+        public string Text { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";
+            
+            if (string.IsNullOrWhiteSpace(this.Href))
+            {
+                throw new ArgumentException("Href property is null or an empty string");
+            }
+            else if (!this.Href.StartsWith("#") && !Uri.IsWellFormedUriString(this.Href, UriKind.RelativeOrAbsolute))
+            {
+                throw new FormatException("Invalid format for Href");
+            }
+
+            var anchorTag = new TagBuilder("a");
+            anchorTag.Attributes.Add("href", this.Href);
+            if (string.IsNullOrWhiteSpace(this.Text))
+            {
+                var content = await output.GetChildContentAsync();
+                anchorTag.InnerHtml.AppendHtml(content);
+            }
+            else
+            {
+                anchorTag.InnerHtml.Append(this.Text);
+            }
+
+            output.Content.AppendHtml(anchorTag);
+        }
+    }
+}
---
+++ NELCCG.NHSUKFrontend.Razor/TagHelpers/ErrorSummaryTagHelper.cs
@@ -0,0 +1,98 @@
+﻿namespace NELCCG.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System;
+    using System.Collections.Generic;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+
+    [HtmlTargetElement("nhsuk-error-summary")]
+    [RestrictChildren("nhsuk-error-summary-list-item")]
+    public class ErrorSummaryTagHelper : TagHelper
+    {
+        /// <summary>
+        /// Any additional css classes to apply.
+        /// </summary>
+        [HtmlAttributeName("classes")]
+        public IEnumerable<string> CssClasses { get; set; } = Array.Empty<string>();
+        /// <summary>
+        /// Description of the errors.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Encode the description if it may not be safe content.
+        /// </summary>
+        public bool EncodeDescription { get; set; }
+        /// <summary>
+        /// Encode the heading if it may not be safe content.
+        /// </summary>
+        public bool EncodeHeading { get; set; }
+        /// <summary>
+        /// Set the heading text.
+        /// </summary>
+        public string Heading { get; set; } = "There is a problem";
+        /// <summary>
+        /// Set the heading level.
+        /// </summary>
+        public int HeadingLevel { get; set; } = 2;
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.AddClass("nhsuk-error-summary", HtmlEncoder.Default);
+
+            foreach (var cssClass in this.CssClasses)
+            {
+                output.AddClass(cssClass, HtmlEncoder.Default);
+            }
+
+            output.Attributes.Add("aria-labelledby", "error-summary-title");
+            output.Attributes.Add("role", "alert");
+            output.Attributes.Add("tabindex", "-1");
+
+            var headingTag = new TagBuilder($"h{this.HeadingLevel}");
+            headingTag.AddCssClass("nhsuk-error-summary__title");
+            headingTag.Attributes.Add("id", "error-summary-title");
+            if (this.EncodeHeading)
+            {
+                headingTag.InnerHtml.Append(this.Heading);
+            }
+            else
+            {
+                headingTag.InnerHtml.AppendHtml(this.Heading);
+            }
+
+            output.Content.AppendHtml(headingTag);
+
+            var contentContainer = new TagBuilder("div");
+            contentContainer.AddCssClass("nhsuk-error-summary__body");
+            if (!string.IsNullOrWhiteSpace(this.Description))
+            {
+                var descriptionTag = new TagBuilder("p");
+                if (this.EncodeDescription)
+                {
+                    descriptionTag.InnerHtml.Append(this.Description);
+                }
+                else
+                {
+                    descriptionTag.InnerHtml.AppendHtml(this.Description);
+                }
+
+                contentContainer.InnerHtml.AppendHtml(descriptionTag);
+            }
+
+            var listContainer = new TagBuilder("ul");
+            listContainer.AddCssClass("nhsuk-list");
+            listContainer.AddCssClass("nhsuk-error-summary__list");
+
+            var content = await output.GetChildContentAsync();
+            listContainer.InnerHtml.AppendHtml(content);
+
+            contentContainer.InnerHtml.AppendHtml(listContainer);
+
+            output.Content.AppendHtml(contentContainer);
+        }
+    }
+}
