1c1501ad7e2631fc5c3c3441d63c96fe39475efc
Sam Collett <sam.collett@nhs.net>
2021-08-17 11:19:14 +0100

    feat: add list tag helper

---
+++ NELCCG.NHSUKFrontend.Razor/TagHelpers/ListTagHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace NELCCG.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("nhsuk-list")]
+    [RestrictChildren("li")]
+    public class ListTagHelper : TagHelper
+    {
+        public bool OrderedList { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = this.OrderedList ? "ol" : "ul";
+            output.AddClass("nhsuk-list", HtmlEncoder.Default);
+        }
+    }
+}
