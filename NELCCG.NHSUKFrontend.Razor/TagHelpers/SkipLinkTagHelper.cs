247dc745fdecf106e971079f5196689e1b38a20e
Sam Collett <sam.collett@nhs.net>
2021-08-20 18:17:10 +0100

    feat: add skip link tag helper

---
+++ NELCCG.NHSUKFrontend.Razor/TagHelpers/SkipLinkTagHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace NELCCG.NHSUKFrontend.Razor.TagHelpers
+{
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Text.Encodings.Web;
+
+    [HtmlTargetElement("nhsuk-skip-link")]
+    public class SkipLinkTagHelper : TagHelper
+    {
+        public string Href { get; set; } = "#maincontent";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.AddClass("nhsuk-skip-link", HtmlEncoder.Default);
+            output.Attributes.SetAttribute("href", this.Href);
+        }
+    }
+}
